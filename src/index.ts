b7929b55b9e4788911364d853fe7ce48dcdb183d
AroLeaf <starleafofa@gmail.com>
2022-10-11 11:34:41 +0200

    [fix] export types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from 'discord.js';
 export * from './bot.js';
 export * from './commands';
 export * from './events';
+export * from './types';
 
 export * as util from './util.js';
 export * as logging from './logging.js';
