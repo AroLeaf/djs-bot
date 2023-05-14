1b211e429b797f6a9234807223db5d476ca7bb51
AroLeaf <starleafofa@gmail.com>
2023-05-14 07:43:53 +0200

    [fix] there was no way to create subcommands

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,11 @@
 export * from './bot';
 export * from './command';
+export * from './components';
 export * from './event';
+export * from './messageCommand';
+export * from './modal';
+// export * from './module';
 export * from './prefixCommand';
 export * from './slashCommand';
+export * from './subCommand';
+export * from './userCommand';
