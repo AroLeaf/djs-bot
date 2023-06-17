b754f6e449976a80087b10aee52b506be3476d50
AroLeaf <starleafofa@gmail.com>
2023-06-17 10:00:23 +0200

    [fix] for some reason recursive readdir includes dirs? weird, anyway, fixed it

--- src/loader.ts
+++ src/loader.ts
@@ -5,14 +5,15 @@ import path from 'path';
 export default class Loader {
   static async import(dir: string) {
     const absolutePath = path.resolve(dir);
-    const files = await fsPromises.readdir(absolutePath, { recursive: true });
+    const files = await fsPromises.readdir(absolutePath, { recursive: true, withFileTypes: true })
+      .then(files => files.filter(file => file.isFile()).map(file => file.name));
     const modules = await Promise.all(files.map(file => import(path.join(absolutePath, file))));
     return modules.map(module => module.default).filter(m => m);
   }
 
   static require(dir: string) {
     const absolutePath = path.resolve(dir);
-    const files = <string[]>fs.readdirSync(absolutePath, { recursive: true });
+    const files = fs.readdirSync(absolutePath, { recursive: true, withFileTypes: true }).filter(file => file.isFile()).map(file => file.name);
     const modules = files.map(file => require(path.join(absolutePath, file)));
     return modules.map(module => module.default || module).filter(m => m);
   }
