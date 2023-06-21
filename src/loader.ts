72220db1adc3d4a80ccad1f529aa81607971ceb1
AroLeaf <starleafofa@gmail.com>
2023-06-21 17:35:41 +0200

    [fix] I should need to do this

--- src/loader.ts
+++ src/loader.ts
@@ -5,15 +5,16 @@ import path from 'path';
 export default class Loader {
   static async import(dir: string) {
     const absolutePath = path.resolve(dir);
-    const files = await fsPromises.readdir(absolutePath, { recursive: true, withFileTypes: true })
-      .then(files => files.filter(file => file.isFile()).map(file => file.name));
+    const files = await <Promise<string[]>>fsPromises.readdir(absolutePath, { recursive: true })
+      .then(files => files.filter(file => file.endsWith('.js') && !file.endsWith('.cjs')));
     const modules = await Promise.all(files.map(file => import(path.join(absolutePath, file))));
     return modules.map(module => module.default).filter(m => m);
   }
 
   static require(dir: string) {
     const absolutePath = path.resolve(dir);
-    const files = fs.readdirSync(absolutePath, { recursive: true, withFileTypes: true }).filter(file => file.isFile()).map(file => file.name);
+    const files = (<string[]>fs.readdirSync(absolutePath, { recursive: true }))
+      .filter(file => file.endsWith('js') && !file.endsWith('mjs'));
     const modules = files.map(file => require(path.join(absolutePath, file)));
     return modules.map(module => module.default || module).filter(m => m);
   }
