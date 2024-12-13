67cdcd42b5146537fe2851150e2d50e9bee7ef30
Leaf <starleafofa@gmail.com>
2024-12-13 14:37:39 +0100

    [feat] add option to allow loading TS files directly

--- src/loader.ts
+++ src/loader.ts
@@ -3,18 +3,18 @@ import fs from 'fs';
 import path from 'path';
 
 export default class Loader {
-  static async import(dir: string) {
+  static async import(dir: string, allowTS = false) {
     const absolutePath = path.resolve(dir);
     const files = await <Promise<string[]>>fsPromises.readdir(absolutePath, { recursive: true })
-      .then(files => files.filter(file => file.endsWith('.js') && !file.endsWith('.cjs')));
+      .then(files => files.filter(file => ['.js', '.mjs'].concat(allowTS ? ['.ts'] : []).includes(path.extname(file))));
     const modules = await Promise.all(files.map(file => import(path.join(absolutePath, file))));
     return modules.map(module => module.default).filter(m => m);
   }
 
-  static require(dir: string) {
+  static require(dir: string, allowTS = false) {
     const absolutePath = path.resolve(dir);
     const files = (<string[]>fs.readdirSync(absolutePath, { recursive: true }))
-      .filter(file => file.endsWith('js') && !file.endsWith('mjs'));
+      .filter(file => ['.js', '.cjs'].concat(allowTS ? ['.ts'] : []).includes(path.extname(file)));
     const modules = files.map(file => require(path.join(absolutePath, file)));
     return modules.map(module => module.default || module).filter(m => m);
   }
